946206cb8e0717464ed1fc81d46f8f400c848da4
lilbenji03 <walibenjamin03@gmail.com>
2026-03-05 12:26:59 +0300

    fixed code rabbit suggestion

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
    <header>
      <div className="main-container inner">
         <Link href="/">
-            <Image src="logo.svg" alt="CoinPulse Logo" width={132} height={40} />
+            <Image src="/logo.svg" alt="CoinPulse Logo" width={132} height={40} />
         </Link>
         <nav>
             <Link href='/' className= {cn('nav-link', { 'is-active': Pathname === '/', 'is-home': true })}>Home</Link>
